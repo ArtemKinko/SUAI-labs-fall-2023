01c61a993fea1a97c484896519ee54ea8fe5b94d
ArtemKinko <artem.kinko.dev@gmail.com>
2023-10-20 03:20:21 +0300

    Написана метод МАИ+ для решения ЛР3 по Системному анализу

--- System analysis/LAB-3 SA-MAI/LAB-3 SA-MAI.py
+++ System analysis/LAB-3 SA-MAI/LAB-3 SA-MAI.py
@@ -1,6 +1,8 @@
 # Вариант №8.
 import fractions
 import io
+import math
+
 from tabulate import tabulate
 
 # C1. Цена
@@ -41,7 +43,6 @@ def create_weight_alternative_table(cr_table, criteria):
 
 
 def calculate_total_coefs(tl_weights):
-    print('tl_weights:', tl_weights)
     return [sum([tl_weights[0][j] * tl_weights[i][j] for j in range(len(tl_weights[0]))])
             for i in range(1, len(tl_weights))]
 
@@ -57,6 +58,55 @@ def is_consistence(table, weights):
     consistence_ratio_index = consistence_index / consistence_random[len(table)]
     print('Индекс отношения согласованности:', consistence_ratio_index)
     return consistence_ratio_index < 0.1
+
+def calculate_mai_plus(cr_weights, al_weights, names):
+    locals_table = []
+    for table in al_weights:
+        locals = []
+        for i in range(len(al_weights[0])):
+            local_row = []
+            for j in range(len(al_weights[0])):
+                local_row.append([table[i] / (table[i] + table[j]), table[j] / (table[i] + table[j])])
+            locals.append(local_row)
+        locals_table.append(locals)
+
+    for i in range(len(locals_table)):
+        print("Локальные ВКА первого уровня для критерия:", names[i])
+        for local in locals_table[i]:
+            test = [" ; ".join([str(round(x, 2)) for x in elem]) for elem in local]
+            print("\t | \t".join(test))
+
+    vka_2 = []
+    for i in range(len(locals_table[0])):
+        temp_row = []
+        for j in range(len(locals_table[0])):
+            elem_x, elem_y = 0, 0
+            for crit in range(len(cr_weights)):
+                elem_x += cr_weights[crit] * locals_table[crit][i][j][0]
+                elem_y += cr_weights[crit] * locals_table[crit][i][j][1]
+            temp_row.append([elem_x, elem_y])
+        vka_2.append(temp_row)
+
+    print("\nЛокальные ВКА второго уровня:")
+    for local in vka_2:
+        test = [" ; ".join([str(round(x, 2)) for x in elem]) for elem in local]
+        print("\t | \t".join(test))
+
+    vka_2 = [[[elem[0] / (elem[0] + elem[1]), elem[1] / (elem[0] + elem[1])] for elem in row] for row in vka_2]
+    globs = [pow(math.prod([x[0] for x in row]), 1 / len(vka_2)) for row in vka_2]
+
+    print("\nНормированные локальные ВКА второго уровня:")
+    for local in vka_2:
+        test = [" ; ".join([str(round(x, 2)) for x in elem]) for elem in local]
+        print("\t | \t".join(test))
+
+    print("\nГлобальные значения ВКА по геометрическому среднему:")
+    print(globs)
+    globs = [elem / sum(globs) for elem in globs]
+    print("\nНормированные глобальные значения ВКА по геометрическому среднему:")
+    print(globs)
+    return globs
+
 
 def solve(criteria_table_path, number_of_criterias):
     criteria_names = []
@@ -88,7 +138,7 @@ def solve(criteria_table_path, number_of_criterias):
     alternative_tables = []
     alternative_weights = []
     for i in range(number_of_criterias):
-        a_table, a_weights = calculate_weights("tables/alternative_c1.txt")
+        a_table, a_weights = calculate_weights("tables/alternative_c" + str(i + 1) + ".txt")
         alternative_tables.append(a_table)
         alternative_weights.append(a_weights)
         names_for_alternative = [criteria_names[i]] + alternative_names + ['Нормированные веса']
@@ -106,10 +156,26 @@ def solve(criteria_table_path, number_of_criterias):
         else:
             print("Матрица согласована!\n\n")
 
+    print("Расчет с помощью МАИ+")
+
+    globs = calculate_mai_plus(n_weights, alternative_weights, criteria_names)
     total = create_weight_alternative_table(n_weights, alternative_weights)
-    print('total', total)
+
     result = calculate_total_coefs(total)
-    print(result)
+
+    print("\nМатрица локальных весов критериев и альтернатив с глобальными весами (по МАИ):")
+    alternative_names.insert(0, "")
+    names_for_table_criteria.insert(-1, "Глобальные веса")
+    for i in range(len(total)):
+        if i != 0:
+            total[i] += [result[i - 1]]
+        total[i] = [alternative_names[i]] + [str(elem) for elem in total[i]]
+    print(tabulate(total, names_for_table_criteria, 'grid'))
+    print("Согласно МАИ, лучшая альтернатива:", alternative_names[result.index(max(result)) + 1])
+
+    print()
+
+    print("Согласно МАИ+, лучшая альтернатива:", alternative_names[globs.index(max(globs)) + 1])
 
 
 solve("tables/criteria_table.txt", 7)
